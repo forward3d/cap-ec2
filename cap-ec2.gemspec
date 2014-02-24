81f94301559e488f6fbfee9317a74e83a1894743
Robert Coleman <github@robert.net.nz>
2014-02-24 16:41:42 +1300

    Update README and CHANGELOG, bump version for release

--- cap-ec2.gemspec
+++ cap-ec2.gemspec
@@ -6,8 +6,8 @@ require 'cap-ec2/version'
 Gem::Specification.new do |spec|
   spec.name          = "cap-ec2"
   spec.version       = CapEC2::VERSION
-  spec.authors       = ["Andy Sykes"]
-  spec.email         = ["github@tinycat.co.uk"]
+  spec.authors       = ["Andy Sykes", "Robert Coleman"]
+  spec.email         = ["github@tinycat.co.uk", "github@robert.net.nz"]
   spec.description   = %q{Cap-EC2 is used to generate Capistrano namespaces and tasks from Amazon EC2 instance tags, dynamically building the list of servers to be deployed to.}
   spec.summary       = %q{Cap-EC2 is used to generate Capistrano namespaces and tasks from Amazon EC2 instance tags, dynamically building the list of servers to be deployed to.}
   spec.homepage      = "https://github.com/forward3d/cap-ec2"
@@ -20,7 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
-  
+
   spec.add_dependency "aws-sdk"
   spec.add_dependency "capistrano", ">= 3.0"
   spec.add_dependency "terminal-table"
--- lib/cap-ec2/ec2-handler.rb
+++ lib/cap-ec2/ec2-handler.rb
@@ -1,7 +1,7 @@
 module CapEC2
   class EC2Handler
     include CapEC2::Utils
-    
+
     def initialize
       load_config
       configured_regions = get_regions(fetch(:ec2_region))
@@ -10,7 +10,7 @@ module CapEC2
         @ec2[region] = ec2_connect(region)
       end
     end
-    
+
     def ec2_connect(region=nil)
       AWS::EC2.new(
         access_key_id: fetch(:ec2_access_key_id),
@@ -18,13 +18,13 @@ module CapEC2
         region: region
       )
     end
-    
+
     def status_table
       CapEC2::StatusTable.new(
         defined_roles.map {|r| get_servers_for_role(r)}.flatten.uniq {|i| i.instance_id}
       )
     end
-    
+
     def server_names
       puts defined_roles.map {|r| get_servers_for_role(r)}
                    .flatten
@@ -32,7 +32,7 @@ module CapEC2
                    .map {|i| i.tags["Name"]}
                    .join("\n")
     end
-    
+
     def instance_ids
       puts defined_roles.map {|r| get_servers_for_role(r)}
                    .flatten
@@ -40,35 +40,35 @@ module CapEC2
                    .map {|i| i.instance_id}
                    .join("\n")
     end
-    
+
     def defined_roles
       Capistrano::Configuration.env.send(:servers).send(:available_roles)
     end
-    
+
     def stage
       Capistrano::Configuration.env.fetch(:stage).to_s
     end
-    
+
     def application
       Capistrano::Configuration.env.fetch(:application).to_s
     end
-    
+
     def tag(tag_name)
       "tag:#{tag_name}"
     end
-    
+
     def get_servers_for_role(role)
       servers = []
       @ec2.each do |_, ec2|
         instances = ec2.instances
           .filter(tag(project_tag), application)
           .filter('instance-state-code', '16')
-        servers << instances.select do |i| 
+        servers << instances.select do |i|
           i.tags[roles_tag] =~ /,{0,1}#{role}(,|$)/ && i.tags[stages_tag] =~ /,{0,1}#{stage}(,|$)/
         end
       end
       servers.flatten
     end
-    
+
   end
 end
