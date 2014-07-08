a79bdc6a378723bf2c5694107223e967741284ee
Tom Conroy <conroy@typography.com>
2014-07-08 16:54:40 -0400

    Add ec2_server method to automatically add roles

--- lib/cap-ec2/capistrano.rb
+++ lib/cap-ec2/capistrano.rb
@@ -14,6 +14,7 @@ load File.expand_path("../tasks/ec2.rake", __FILE__)
 module Capistrano
   module DSL
     module Ec2
+      include CapEC2::Utils
 
       def ec2_handler
         @ec2_handler ||= CapEC2::EC2Handler.new
@@ -22,6 +23,13 @@ module Capistrano
       def ec2_role(name, options={})
         ec2_handler.get_servers_for_role(name).each do |server|
           env.role(name, CapEC2::Utils.contact_point(server), options)
+        end
+      end
+
+      def ec2_server(options={})
+        ec2_handler.get_servers.each do |server|
+          roles = server.tags[roles_tag] || ''
+          env.server(CapEC2::Utils.contact_point(server), options.merge(roles: roles.split(',')))
         end
       end
 
