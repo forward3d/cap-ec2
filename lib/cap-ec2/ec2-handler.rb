a63015a63f8587508156468ea133c624ef9a074c
Pedro Garrett <pedro.garrett@emediaweb.com>
2015-08-03 15:36:48 -0400

    Allow instance_name env var to filter instances

--- lib/cap-ec2/ec2-handler.rb
+++ lib/cap-ec2/ec2-handler.rb
@@ -71,7 +71,8 @@ module CapEC2
             (fetch(:ec2_filter_by_status_ok?) ? instance_status_ok?(i) : true)
         end
       end
-      servers.flatten.sort_by {|s| s.tags["Name"] || ''}
+      servers.flatten.sort_by {|s| s.tags["Name"] || ''}.
+        select{|i| !ENV['instance_name'] || (ENV['instance_name'] == i.tags['Name'])}
     end
 
     def get_server(instance_id)
